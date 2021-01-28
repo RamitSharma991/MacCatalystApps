be8bfc463067edf8f5eda43a1834acb58ad0ceb9
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2021-01-28 13:29:53 +0530

    initial commit

---
+++ KeepingFit/KeepingFit/KeepingFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepingFitApp.swift
+//  KeepingFit
+//
+//  Created by Ramit sharma on 28/01/21.
+//
+
+import SwiftUI
+
+@main
+struct KeepingFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
